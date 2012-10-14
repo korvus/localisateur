4028fb03860c9f68e3c2501255b14ae2e3dcd2ec
Simon Ertel <ertel.simon@gmail.com>
2012-10-14 19:48:12 +0200

    Exemples
    Ensemble des fichiers pour l'appli

---
+++ map.js
@@ -0,0 +1,33 @@
+function initialisation(){
+
+	origin = new google.maps.LatLng(0,0);
+
+	var optionsCarte = {
+		zoom: 5,
+		center: origin,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	
+	maCarte = new google.maps.Map(document.getElementById("map"), optionsCarte);
+	
+	marker = new google.maps.Marker({
+		map:maCarte,
+		draggable:true,
+		position: origin
+	});
+	
+	google.maps.event.addListener(marker,'dragend',afficherUneInfobulle);
+}
+
+function afficherUneInfobulle(){
+
+	var aAfficher = "Latitude : "+marker.position.Xa+'<br />Longtitude : '+marker.position.Ya+"<br />new google.maps.LatLng("+marker.position.Xa+","+marker.position.Ya+");";
+
+	var infowindow = new google.maps.InfoWindow({
+		content: aAfficher
+	});
+
+	infowindow.open(maCarte,marker);
+}
+
+window.onload = initialisation;
